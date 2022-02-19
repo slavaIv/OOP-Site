85024cf0c6f3c51418d173f1fcf645b80eb27310
slavaIv <slavchiky@gmail.com>
2022-02-19 23:56:51 +0100

    1. video player was added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Slider from "./modules/slider"
+import Video from "./modules/video"
 
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const video = new Video(".showup .play", '.overlay');
+    video.init();
 })
